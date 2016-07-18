8b74bb4373f5d8a19f6f9e165a985e626aa15022
wwalser <waw325@gmail.com>
2016-07-18 18:28:12 -0400

    #3 Update repo to 0.0.12

--- chartist-plugin-tooltip.js
+++ chartist-plugin-tooltip.js
@@ -1,87 +1,189 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['chartist', 'jquery'], function (chartist, jquery) {
-            return (root.returnExportsGlobal = factory(chartist, jquery));
-        });
-    } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like enviroments that support module.exports,
-        // like Node.
-        module.exports = factory(require('chartist'), require('jquery'));
-    } else {
-        root['Chartist.plugins.tooltip'] = factory(root.chartist, root.jquery);
-    }
-}(this, function (Chartist, $) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(["chartist"], function (Chartist) {
+      return (root.returnExportsGlobal = factory(Chartist));
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like enviroments that support module.exports,
+    // like Node.
+    module.exports = factory(require("chartist"));
+  } else {
+    root['Chartist.plugins.tooltips'] = factory(Chartist);
+  }
+}(this, function (Chartist) {
 
-    /**
-     * This Chartist tooltip plugin is a modified version of
-     * https://github.com/Globegitter/chartist-plugin-tooltip.
-     *
-     */
+  /**
+   * Chartist.js plugin to display a data label on top of the points in a line chart.
+   *
+   */
+  /* global Chartist */
+  (function (window, document, Chartist) {
     'use strict';
 
     var defaultOptions = {
-        valueTransform: Chartist.noop,
-        seriesName: true // Show name of series in tooltip.
+      currency: undefined,
+      tooltipOffset: {
+        x: 0,
+        y: -20
+      },
+      appendToBody: false,
+      class: undefined,
+      pointClass: 'ct-point'
     };
 
     Chartist.plugins = Chartist.plugins || {};
-
     Chartist.plugins.tooltip = function (options) {
+      options = Chartist.extend({}, defaultOptions, options);
 
-        options = Chartist.extend({}, defaultOptions, options);
+      return function tooltip(chart) {
+        var tooltipSelector = options.pointClass;
+        if (chart instanceof Chartist.Bar) {
+          tooltipSelector = 'ct-bar';
+        } else if (chart instanceof Chartist.Pie) {
+          // Added support for donut graph
+          if (chart.options.donut) {
+            tooltipSelector = 'ct-slice-donut';
+          } else {
+            tooltipSelector = 'ct-slice-pie';
+          }
+        }
 
-        return function tooltip(chart) {
+        var $chart = chart.container;
+        var $toolTip = $chart.querySelector('.chartist-tooltip');
+        if (!$toolTip) {
+          $toolTip = document.createElement('div');
+          $toolTip.className = (!options.class) ? 'chartist-tooltip' : 'chartist-tooltip ' + options.class;
+          if (!options.appendToBody) {
+            $chart.appendChild($toolTip);
+          } else {
+            document.body.appendChild($toolTip);
+          }
+        }
+        var height = $toolTip.offsetHeight;
+        var width = $toolTip.offsetWidth;
 
-            var tooltipSelector = '.ct-point';
-            if (chart instanceof Chartist.Bar) {
-                tooltipSelector = '.ct-bar';
-            } else if (chart instanceof Chartist.Pie) {
-                tooltipSelector = '[class^=ct-slice]';
+        hide($toolTip);
+
+        function on(event, selector, callback) {
+          $chart.addEventListener(event, function (e) {
+            if (!selector || hasClass(e.target, selector))
+              callback(e);
+          });
+        }
+
+        on('mouseover', tooltipSelector, function (event) {
+          var $point = event.target;
+          var tooltipText = '';
+
+          var isPieChart = (chart instanceof Chartist.Pie) ? $point : $point.parentNode;
+          var seriesName = (isPieChart) ? $point.parentNode.getAttribute('ct:meta') || $point.parentNode.getAttribute('ct:series-name') : '';
+          var meta = $point.getAttribute('ct:meta') || seriesName || '';
+          var hasMeta = !!meta;
+          var value = $point.getAttribute('ct:value');
+
+          if (options.transformTooltipTextFnc && typeof options.transformTooltipTextFnc === 'function') {
+            value = options.transformTooltipTextFnc(value);
+          }
+
+          if (options.tooltipFnc && typeof options.tooltipFnc === 'function') {
+            tooltipText = options.tooltipFnc(meta, value);
+          } else {
+            if (options.metaIsHTML) {
+              var txt = document.createElement("textarea");
+              txt.innerHTML = meta;
+              meta = txt.value;
             }
 
-            var $chart = $(chart.container),
-                $toolTip = $chart
-                .append('<div class="ct-tooltip"></div>')
-                .find('.ct-tooltip')
-                .hide();
+            meta = '<span class="chartist-tooltip-meta">' + meta + '</span>';
 
-            $chart.on('mouseenter', tooltipSelector, function() {
-                var $point = $(this),
-                    seriesName = $point.parent().attr('ct:series-name'),
-                    tooltipText = '';
-
-                if (options.seriesName && seriesName) {
-                    tooltipText += seriesName + '<br>';
+            if (hasMeta) {
+              tooltipText += meta + '<br>';
+            } else {
+              // For Pie Charts also take the labels into account
+              // Could add support for more charts here as well!
+              if (chart instanceof Chartist.Pie) {
+                var label = next($point, 'ct-label');
+                if (label) {
+                  tooltipText += text(label) + '<br>';
                 }
+              }
+            }
 
-                if ($point.attr('ct:meta')) {
-                    tooltipText += $point.attr('ct:meta') + '<br>';
-                }
+            if (value) {
+              if (options.currency) {
+                value = options.currency + value.replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,");
+              }
+              value = '<span class="chartist-tooltip-value">' + value + '</span>';
+              tooltipText += value;
+            }
+          }
 
-                var value = $point.attr('ct:value') || '0';
+          if(tooltipText) {
+            $toolTip.innerHTML = tooltipText;
+            setPosition(event);
+            show($toolTip);
 
-                tooltipText += options.valueTransform(value);
+            // Remember height and width to avoid wrong position in IE
+            height = $toolTip.offsetHeight;
+            width = $toolTip.offsetWidth;
+          }
+        });
 
-                $toolTip.html(tooltipText).show();
-            });
+        on('mouseout', tooltipSelector, function () {
+          hide($toolTip);
+        });
 
-            $chart.on('mouseleave', tooltipSelector, function() {
-                $toolTip.hide();
-            });
+        on('mousemove', null, function (event) {
+          setPosition(event);
+        });
 
-            $chart.on('mousemove', function(event) {
-                $toolTip.css({
-                    left: (event.offsetX || event.originalEvent.layerX) - $toolTip.width() / 2 - 10,
-                    top: (event.offsetY || event.originalEvent.layerY) - $toolTip.height() - 40
-                });
-            });
-
-        };
-
+        function setPosition(event) {
+          // For some reasons, on FF, we can't rely on event.offsetX and event.offsetY,
+          // that's why we prioritize event.layerX and event.layerY
+          // see https://github.com/gionkunz/chartist-js/issues/381
+          height = height || $toolTip.offsetHeight;
+          width = width || $toolTip.offsetWidth;
+          if (!options.appendToBody) {
+            $toolTip.style.top = (event.layerY || event.offsetY) - height + options.tooltipOffset.y + 'px';
+            $toolTip.style.left = (event.layerX || event.offsetX) - width / 2 + options.tooltipOffset.x + 'px';
+          } else {
+            $toolTip.style.top = event.pageY - height  + options.tooltipOffset.y + 'px';
+            $toolTip.style.left = event.pageX - width / 2 + options.tooltipOffset.x + 'px';
+          }
+        }
+      }
     };
 
-    return Chartist.plugins.tooltip;
+    function show(element) {
+      if(!hasClass(element, 'tooltip-show')) {
+        element.className = element.className + ' tooltip-show';
+      }
+    }
+
+    function hide(element) {
+      var regex = new RegExp('tooltip-show' + '\\s*', 'gi');
+      element.className = element.className.replace(regex, '').trim();
+    }
+
+    function hasClass(element, className) {
+      return (' ' + element.getAttribute('class') + ' ').indexOf(' ' + className + ' ') > -1;
+    }
+
+    function next(element, className) {
+      do {
+        element = element.nextSibling;
+      } while (element && !hasClass(element, className));
+      return element;
+    }
+
+    function text(element) {
+      return element.innerText || element.textContent;
+    }
+
+  } (window, document, Chartist));
+
+  return Chartist.plugins.tooltips;
 
 }));
